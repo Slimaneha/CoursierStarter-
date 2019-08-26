aa9045bf774787994a965a2c9cb1ca7ce3ad95cb
Slimane <slimanehassani@hotmail.fr>
2019-08-26 21:32:17 +0300

    Ajouts Article.js

---
+++ js/Article.js
@@ -0,0 +1,269 @@
+// Alcool 
+
+var A1 = {
+    nomProduit: "Jack-Daniels",
+    type: "Wisky", 
+    quantité: "70 CL",
+    volt: "40°",
+    prix: "27€",
+    information: "Old n°7 est la recette mythique et originale de Jack Daniel's depuis 1866. Il conserve ce goût unique grâce à la filtration sur charbon de bois d'érable, qui fait de lui un Tennessee Whiskey.. JACK DANIEL'S OLD N°7 - 70cl - 40°",
+}
+
+    var A2 = {
+        nomProduit: "William Lawson",
+        type: "Wisky",
+        volt: "40°",  
+        quantité: "70Cl",
+        prix: "20€",
+        information: "Blended scotch: il est composé notamment du single malt The Deveron et de whiskies de grain.. Whisky blend moins de 12 d'âge",
+    
+
+
+
+
+    }
+
+
+    var A3 = {
+        nomProduit: "William Peel",
+        type: "Wisky",
+        volt: "40°",  
+        quantité: "70Cl",
+        prix: "17€",
+        information: "Blend scotch Whisky William Peel 40° 70cl. Whisky",
+    
+
+
+
+
+    }
+
+
+    var A4 = {
+        nomProduit: " Eristoff vodka",
+        type: "Vodka",
+        volt: "37°",  
+        quantité: "70Cl",
+        prix: "18€",
+        information: "La vodka Eristoff® 100% Premium originaire de Géorgie, la « Terre des loups », est connue dans le monde entier pour sa pureté et son goût depuis plus de 200 ans. Elle doit son nom à la noble famille Eristhavi, qui en a créé la recette originale en 1806.. Vodka",
+    
+
+
+
+
+    }
+
+    var A5 = {
+        nomProduit: " Poliakov vodka",
+        type: "Vodka",
+        volt: "37°",  
+        quantité: "70Cl",
+        prix: "18€",
+        information: "Elaboré à partir d’une sélection des meilleurs grains, la Vodka POLIAKOV dévoile une transparence cristalline, elle se caractérise par un accord parfait entre pureté et fraîcheur tout en développant des arômes subtils provenant de la sélection des meilleurs grains et de sa triple distillation. Une texture riche en bouche et une finale franche et délicate. Cette boisson de dégustation est idéale pour les cocktails. La Vodka POLIAKOV est issue d’une triple distillation des meilleures céréales sélectionnées (blé, orge, seigle) et d’un savoir-faire ancestrale, qui lui confère pureté et fraîcheur. La Vodka POLIAKOV titre à 37,5% suite à l’adjonction d’eau de source de la plus grande pureté.. Vodka",
+    
+
+
+
+
+    }
+
+    var  A6  = {
+        nomProduit: " Absolut blue vodka ",
+        type: "Vodka",
+        volt: "40°",  
+        quantité: "70Cl",
+        prix: "23€",
+        information: "A base de blé d’hiver et d’ingrédients d’origine 100% naturelle, la gamme Absolut Vodka est élaborée selon la méthode de distillation continue par laquelle la vodka est distillée plus d’une centaine de fois et qui lui permet d’atteindre un niveau de pureté exceptionnel. Absolut Vodka offre une gamme variée qui se compose d’une vodka nature ainsi que de vodkas aromatisées qui vous permettront de créer des cocktails savoureux et audacieux.. Vodka",
+    
+
+
+
+
+    }
+
+
+    var  A7  = {
+        nomProduit: " 3 Monts bière blonde ",
+        type: "Biére",
+        volt: "8,5°",  
+        quantité: "75Cl",
+        prix: "4€",
+        information: "La 3 Monts est une bière historique du Nord de la France, à la saveur d'antan des bières que l'on tirait du tonneau de bois cerclé et qui remplissait nombre de chopes dans les estaminets des Flandres! Brassée par la Brasserie Saint Sylvestre à Saint Sylvestre Cappel. Brassée par infusion ascendante comme jadis, avec des houblons flamands, puis fermentée avec des levures hautes, la 3 Monts est une bière de dégustation de grande qualité. Bière de caractère, coiffée d'une mousse serrée et persistante, la 3 Monts est une bière blonde qui dévoile un profil aromatique très riche. En effet, on distingue chez elle une jolie présence de houblons ainsi que des notes fruitées et citronnées.. 3 Monts bière blonde 8,5° bouteille 75cl",
+    
+
+
+
+
+    }
+
+
+    var  A8  = {
+        nomProduit: " Rince Cochon  ",
+        type: "Biére",
+        volt: "8,5°",  
+        quantité: "75Cl",
+        prix: "6€",
+        information: "A base de blé d’hiver et d’ingrédients d’origine 100% naturelle, la gamme Absolut Vodka est élaborée selon la méthode de distillation continue par laquelle la vodka est distillée plus d’une centaine de fois et qui lui permet d’atteindre un niveau de pureté exceptionnel. Absolut Vodka offre une gamme variée qui se compose d’une vodka nature ainsi que de vodkas aromatisées qui vous permettront de créer des cocktails savoureux et audacieux.. Vodka",
+    
+
+
+
+
+    }
+
+
+    var  A9  = {
+        nomProduit: " Leffe  ",
+        type: "Biére",
+        volt: "6,6°",  
+        quantité: "75Cl",
+        prix: "4€",
+        information: "A base de blé d’hiver et d’ingrédients d’origine 100% naturelle, la gamme Absolut Vodka est élaborée selon la méthode de distillation continue par laquelle la vodka est distillée plus d’une centaine de fois et qui lui permet d’atteindre un niveau de pureté exceptionnel. Absolut Vodka offre une gamme variée qui se compose d’une vodka nature ainsi que de vodkas aromatisées qui vous permettront de créer des cocktails savoureux et audacieux.. Vodka",
+    
+
+
+
+
+    }
+
+
+
+
+
+
+
+    function informationArticle() {
+        document.getElementById("Ta1").innerHTML=A1["nomProduit"]
+        document.getElementById("Pa1").innerHTML=A1["prix"]
+
+        document.getElementById("Ta2").innerHTML=A2["nomProduit"]
+        document.getElementById("Pa2").innerHTML=A2["prix"]
+
+
+        document.getElementById("Ta3").innerHTML=A3["nomProduit"]
+        document.getElementById("Pa3").innerHTML=A3["prix"]
+
+        document.getElementById("Ta4").innerHTML=A4["nomProduit"]
+        document.getElementById("Pa4").innerHTML=A4["prix"]
+
+
+        document.getElementById("Ta5").innerHTML=A5["nomProduit"]
+        document.getElementById("Pa5").innerHTML=A5["prix"]
+
+        document.getElementById("Ta6").innerHTML=A7["nomProduit"]
+        document.getElementById("Pa6").innerHTML=A7["prix"]
+
+        document.getElementById("Ta8").innerHTML=A8["nomProduit"]
+        document.getElementById("Pa8").innerHTML=A8["prix"]
+
+        document.getElementById("Ta9").innerHTML=A9["nomProduit"]
+        document.getElementById("Pa9").innerHTML=A9["prix"]
+
+       
+
+
+    }
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Soft 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// epicerie 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// hygenes 
